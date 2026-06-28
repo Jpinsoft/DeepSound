ce8474cb28cedfe43d81cc7f3dad892b6298bdde
Jozef Bátora <Jospin@outlook.sk>
2026-06-28 12:02:25 +0200

    FIX CommandExtensions - default params, version upgrade

--- DeepSoundConsole/ConsoleHelpers/CommandExtensions.cs
+++ DeepSoundConsole/ConsoleHelpers/CommandExtensions.cs
@@ -11,9 +11,9 @@ namespace DeepSoundConsole.ConsoleHelpers
     public static class CommandExtensions
     {
         public static readonly Argument<string> InputFileArgument = new Argument<string>("input-file-or-folder") { Description = "Source carrier audio file or folder into which the application will try to hide secret files." };
-        public static readonly Argument<string> ActionArgument = new Argument<string>("action") { Description = "Action to perform: 'analyze or a' (default), 'encode or e', 'decode or d'." };
+        public static readonly Argument<string> ActionArgument = new Argument<string>("action") { DefaultValueFactory = (_) => "-analyze", Description = "Action to perform: '-analyze or -a' (default), '-encode or -e', '-decode or -d'." };
 
-        public static readonly Option<string> EncodeFormatOpt = new Option<string>("-f", "-F") { Description = "Encoder output audio format: Supported formats: wav (default), flac, ape." };
+        public static readonly Option<string> EncodeFormatOpt = new Option<string>("-f", "-F") { DefaultValueFactory = (_) => "wav", Description = "Encoder output audio format: Supported formats: wav (default), flac, ape." };
         public static readonly Option<string> PassOpt = new Option<string>("-p", "-P") { Description = "Password for AES-256 encryption for use in ENCODE/DECODE modes." };
         public static readonly Option<string[]> SecretFilesOpt = new Option<string[]>("-s", "-S") { AllowMultipleArgumentsPerToken = true, Description = "Secret files: List of input files to hide inside the carrier audio file (space-separated)." };
         public static readonly Option<string> OutDirOpt = new Option<string>("-o", "-O") { Description = "Output directory. (Optional - default is the current working directory.)" };
@@ -22,10 +22,10 @@ namespace DeepSoundConsole.ConsoleHelpers
         public static void SetDescription(this RootCommand rootCommand)
         {
             rootCommand.Description = @"
-EXAMPLE (ANALYZE): DeepSoundConsole.exe ""D:\audio.wav"" analyze
-EXAMPLE (ANALYZE): DeepSoundConsole.exe ""D:\INPUT_FOLDER"" analyze -r
-EXAMPLE (DECODE):  DeepSoundConsole.exe ""D:\audio.wav"" decode -o ""D:\Out"" -p ""*mypass*""
-EXAMPLE (ENCODE):  DeepSoundConsole.exe ""D:\audio.wav"" encode -s ""D:\Secret1.bmp"" ""D:\Secret2.png"" -o ""D:\Out"" -p ""*mypass*"" -f ""ape""";
+EXAMPLE (ANALYZE): DeepSoundConsole.exe ""D:\audio.wav"" -analyze
+EXAMPLE (ANALYZE): DeepSoundConsole.exe ""D:\INPUT_FOLDER"" -analyze -r  (NOTE - Do not use '\' char at the end of a folder name)
+EXAMPLE (DECODE):  DeepSoundConsole.exe ""D:\audio.wav"" -decode -o ""D:\Out"" -p ""*mypass*""
+EXAMPLE (ENCODE):  DeepSoundConsole.exe ""D:\audio.wav"" -encode -s ""D:\Secret1.bmp"" ""D:\Secret2.png"" -o ""D:\Out"" -p ""*mypass*"" -f ""ape""";
         }
 
         public static void SetOptions(this RootCommand rootCommand)
@@ -48,25 +48,26 @@ EXAMPLE (ENCODE):  DeepSoundConsole.exe ""D:\audio.wav"" encode -s ""D:\Secret1.
             {
                 string? action = parseResult.GetValue<string>(CommandExtensions.ActionArgument)?.ToUpper();
 
-                switch (action ?? "ANALYZE")
+                switch (action)
                 {
                     // TODO: Use Dependency Injection for ICommandHandler if more handlers are added in the future
-                    case "A":
-                    case "ANALYZE":
+                    case "-A":
+                    case "-ANALYZE":
                         new AnalyzeHandler().Handle(parseResult);
                         break;
 
-                    case "E":
-                    case "ENCODE":
+                    case "-E":
+                    case "-ENCODE":
                         new EncodeHandler().Handle(parseResult);
                         break;
 
-                    case "D":
-                    case "DECODE":
+                    case "-D":
+                    case "-DECODE":
                         new DecodeHandler().Handle(parseResult);
                         break;
 
-                    default: throw new ArgumentException($"Invalid action: {action}. Supported actions are 'analyze or a', 'encode or e', 'decode or d'.");
+                    default:
+                        throw new ArgumentException($"Invalid action: '{action}'. {CommandExtensions.ActionArgument.Description}");
                 }
             });
         }
